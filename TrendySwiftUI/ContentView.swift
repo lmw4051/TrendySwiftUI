b003eeb8fec26c310dda3de22d6e3494081b8112
David Lee <david.m.lee@ihwic.com>
2021-12-27 01:33:07 +0800

    Adding Guitar Card View

--- TrendySwiftUI/ContentView.swift
+++ TrendySwiftUI/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    VStack(alignment: .leading, spacing: 8.0) {
+      Spacer()
+      Image("GuitarLogo")
+        .resizable(resizingMode: .stretch)
+        .aspectRatio(contentMode: .fit)
+        .frame(width: 26.0, height: 26.0)
+        .cornerRadius(20.0)
+      Text("Guitar")
+        .font(.largeTitle)
+        .fontWeight(.bold)
+      Text("String Instrument")
+        .font(.footnote)
+        .fontWeight(.semibold)
+      Text("The guitar is a fretted musical instrument that typically has six strings.")
+        .font(.footnote)
+        .multilineTextAlignment(.leading)
+        .lineLimit(2)
     }
+    .padding(.all, 20.0)
+    .frame(height: 350.0)
+    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+    .cornerRadius(30.0)
+    .shadow(radius: 20)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
