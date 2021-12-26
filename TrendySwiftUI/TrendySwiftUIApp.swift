06a0b76bcb3e5f39ba46f43cee259b9dca895bbe
David Lee <david.m.lee@ihwic.com>
2021-12-27 01:02:31 +0800

    Initial Commit

---
+++ TrendySwiftUI/TrendySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrendySwiftUIApp.swift
+//  TrendySwiftUI
+//
+//  Created by David on 2021/12/27.
+//
+
+import SwiftUI
+
+@main
+struct TrendySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
