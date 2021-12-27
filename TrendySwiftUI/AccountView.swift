850ce10276ebbf78ef5d92116a243e04f901118d
David Lee <david.m.lee@ihwic.com>
2021-12-27 12:43:07 +0800

    Adding another section for Link

--- TrendySwiftUI/AccountView.swift
+++ TrendySwiftUI/AccountView.swift
@@ -36,14 +36,43 @@ struct AccountView: View {
           }
         }
         .frame(maxWidth: .infinity)
-        .padding()
+        .padding()                
         
         Section {
-          Label("Settings", systemImage: "gear")
-          Label("Billing", systemImage: "creditcard")
-          Label("Help", systemImage: "questionmark")
+          NavigationLink { ContentView() } label: {
+            Label("Settings", systemImage: "gear")
+          }
+          NavigationLink { Text("Billing") } label: {
+            Label("Billing", systemImage: "creditcard")
+          }
+          NavigationLink { ContentView() } label: {
+            Label("Help", systemImage: "questionmark")
+          }
         }
+        .accentColor(.primary)
         .listRowSeparatorTint(.blue)
+        .listRowSeparator(.hidden)
+        
+        Section {
+          Link(destination: URL(string: "https://apple.com")!) {
+            HStack {
+              Label("Website", systemImage: "house")
+              Spacer()
+              Image(systemName: "link")
+                .foregroundColor(.secondary)
+            }
+          }
+          
+          Link(destination: URL(string: "https://youtube.com")!) {
+            HStack {
+              Label("YouTube", systemImage: "tv")
+              Spacer()
+              Image(systemName: "link")
+                .foregroundColor(.secondary)
+            }
+          }
+        }
+        .accentColor(.primary)
         .listRowSeparator(.hidden)
       }
       .listStyle(.insetGrouped)
