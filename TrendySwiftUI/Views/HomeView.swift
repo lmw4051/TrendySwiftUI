4693cf020473dd4acf683adf006e8eee6e94a32a
David Lee <david.m.lee@ihwic.com>
2021-12-27 22:46:02 +0800

    Using GeometryReader to get the data for rotation3DEffect in order to create parallax and rotation 3D transition effect

--- TrendySwiftUI/Views/HomeView.swift
+++ TrendySwiftUI/Views/HomeView.swift
@@ -49,8 +49,25 @@ struct HomeView: View {
   
   var featured: some View {
     TabView {
-      ForEach(courses) { item in
-        FeaturedItem(course: item)
+      ForEach(courses) { course in
+        GeometryReader { proxy in
+          let minX = proxy.frame(in: .global).minX
+          
+          FeaturedItem(course: course)
+            .padding(.vertical, 40)
+            .rotation3DEffect(.degrees(minX / -10), axis: (x: 0, y: 1, z: 0))
+            .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+            .blur(radius: abs(minX / 40))
+            .overlay(
+              Image(course.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 230)
+                .offset(x: 32, y: -80)
+                .offset(x: minX / 2)
+            )
+//          Text("\(proxy.frame(in: .global).minX)")
+        }
       }
     }
     .tabViewStyle(.page(indexDisplayMode: .never))
@@ -64,5 +81,6 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
   static var previews: some View {
     HomeView()
+      .preferredColorScheme(.dark)
   }
 }
