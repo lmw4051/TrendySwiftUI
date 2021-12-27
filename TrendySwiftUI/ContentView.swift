70222d58d6f8834fdd971b8accc38818bf314c8d
David Lee <david.m.lee@ihwic.com>
2021-12-27 11:23:44 +0800

    Use another setting for background instead of using cornerRadius

--- TrendySwiftUI/ContentView.swift
+++ TrendySwiftUI/ContentView.swift
@@ -16,6 +16,8 @@ struct ContentView: View {
         .aspectRatio(contentMode: .fit)
         .frame(width: 26.0, height: 26.0)
         .cornerRadius(20.0)
+        .padding(9)
+        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 20, style: .continuous))
       Text("Acoustic Guitar")
         .font(.largeTitle)
         .fontWeight(.bold)
@@ -34,8 +36,7 @@ struct ContentView: View {
     .padding(.all, 20.0)
     .padding(.vertical, 20)
     .frame(height: 350.0)
-    .background(.ultraThinMaterial)
-    .cornerRadius(30.0)
+    .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
     .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
     .padding(.horizontal, 20)
     .background(
